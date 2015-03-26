fe864698a60d2ce894555ec084f45f60dc0e0799
Brendon Brewer <brendon.brewer@gmail.com>
2015-03-26 16:47:50 -0400

    perturb method

--- code/Distributions/Exponential.cpp
+++ code/Distributions/Exponential.cpp
@@ -1,5 +1,6 @@
 #include "Exponential.h"
 #include "RandomNumberGenerator.h"
+#include "Utils.h"
 
 using namespace DNest3;
 
@@ -11,5 +12,12 @@ Exponential::Exponential()
 void Exponential::fromPrior()
 {
 	mu = 10.*randomU();
+}
+
+double Exponential::perturb()
+{
+	mu += 10.*randh();
+	wrap(mu, 0., 10.);
+	return 0.;
 }
 
