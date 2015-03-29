25b7abe1f72857b4e72d9796275f1e3082b77c48
Brendon J. Brewer <brendon.brewer@gmail.com>
2015-03-30 07:27:02 +1300

    PLot posterior mean

--- code/display.py
+++ code/display.py
@@ -8,10 +8,14 @@ hist(sample[:,0], 100)
 xlabel('Thickness')
 show()
 
+
 ion()
 hold(False)
 for i in xrange(0, sample.shape[0]):
   subplot(2,1,1)
+  hold(False)
+  plot(sample[:,1:(n+1)].mean(axis=0), 'ro-')
+  hold(True)
   plot(sample[i, 1:(n+1)], 'bo-')
   ylim([0, 1])
 
